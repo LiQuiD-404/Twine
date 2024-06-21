b22b2397da7ff60f6376c7a04f58127fa1fdde7a
Shubham Singh <shubhamsingh.exe@gmail.com>
2024-06-21 11:55:06 +0530

    Update index.ts

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -8,7 +8,9 @@ export const SignupValidation = z.object({
   username: z.string().min(2, { message: "Username must be at least 2 characters long." }),
   email: z.string().email({ message: "Invalid email address." }),
   password: z.string().min(8, { message: "Password must be at least 8 characters long." }),
-});
+})
+
+  ;
 
 export const SigninValidation = z.object({
   email: z.string().email({ message: "Invalid email address." }),
@@ -26,9 +28,10 @@ export const ProfileValidation = z.object({
 // ============================================================
 // POST
 // ============================================================
+
 export const PostValidation = z.object({
   caption: z.string().min(5, { message: "Caption must be at least 5 characters long." }).max(2200, { message: "Caption must be at most 2,200 characters long." }),
-  file: z.custom<File[]>().refine(files => files.length > 0, { message: "At least one file is required." }),
+  file: z.custom<File[]>().refine(files => files.length >= 0, { message: "At least one file is required." }),
   location: z.string().min(1, { message: "Location is required." }).max(1000, { message: "Location must be at most 1000 characters long." }),
   tags: z.string().max(200, { message: "Tags must be at most 200 characters long." }).optional(),
 });
