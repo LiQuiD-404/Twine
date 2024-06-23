bfde32da39fe3035ef936a7cf65d7e6a5f988e16
Shubham Singh <shubhamsingh.exe@gmail.com>
2024-06-24 00:09:03 +0530

    Update Home.tsx

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -34,7 +34,16 @@ const Home = () => {
     <div className="flex flex-1">
       <div className="home-container">
         <div className="home-posts">
-          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          <h2 className="h3-bold md:h2-bold text-left w-full">
+            {(() => {
+              const hours = new Date().getHours();
+              if (hours > 12) {
+                return hours > 17 ? "Good Evening!" : "Good Afternoon!";
+              } else {
+                return "Good Morning!";
+              }
+            })()}
+          </h2>
           {isPostLoading && !posts ? (
             <Loader />
           ) : (
